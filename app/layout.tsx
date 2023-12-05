b97247e4298e52ea7bd59750ac2e6ee227617f85
Diriangen Lempira Powell <dlpowell35@gmail.com>
2023-12-05 15:59:14 +0800

    feat: added supabase auth support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { appMetadata } from '@/config/constants/metadata/metadata';
-import AuthProvider from '@/utilities/providers/auth/auth.provider.components';
-import { NextUIProvider } from '@nextui-org/system';
-import CoreProvider from '@/utilities/providers/core.provider';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { appMetadata } from '@/config/constants/metadata/metadata'
+import AuthProvider from '@/utilities/providers/auth/auth.provider.components'
+import { NextUIProvider } from '@nextui-org/system'
+import CoreProvider from '@/utilities/providers/core.provider'
+import { SupabaseProvider } from '@/utilities/providers/backend/supabase'
 
-export const metadata: Metadata = appMetadata;
+export const metadata: Metadata = appMetadata
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <main>
-          <CoreProvider>
-            <AuthProvider>
-              <div>{children}</div>
-            </AuthProvider>
-          </CoreProvider>
+          <SupabaseProvider>
+            <CoreProvider>
+              <AuthProvider>
+                <div>{children}</div>
+              </AuthProvider>
+            </CoreProvider>
+          </SupabaseProvider>
         </main>
       </body>
     </html>
-  );
+  )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,29 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { AUTH_LOGIN, NO_AUTH_LOGIN } from './config/constants/routes/routes';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { AUTH_LOGIN, NO_AUTH_LOGIN } from './config/constants/routes/routes'
 
 export async function middleware(request: NextRequest) {
-  const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
-  const { data, error } = await supabase.auth.getUser();
-  console.log('user is unauthenticated');
-  console.log(data);
-  console.log(error?.message);
+  /* The code is creating a cookie store and using it to create a Supabase client. The `cookies()`
+function is used to retrieve the cookies from the request headers. The `createRouteHandlerClient()`
+function is then called with the `cookies` option set to a function that returns the cookie store.
+This allows the Supabase client to use the cookies from the request. Finally, the `getUser()` method
+is called on the `auth` object of the Supabase client to retrieve the authenticated user's data and
+error, if any. */
+  const cookieStore = cookies()
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+  const { data, error } = await supabase.auth.getUser()
 
   if (data.user && request.nextUrl.pathname.includes(NO_AUTH_LOGIN)) {
-    return NextResponse.redirect(new URL(AUTH_LOGIN, request.url));
+    return NextResponse.redirect(new URL(AUTH_LOGIN, request.url))
   } else if (data.user) {
   } else {
-    console.log('unauthenticated ');
-    return NextResponse.redirect(new URL(NO_AUTH_LOGIN, request.url));
+    return NextResponse.redirect(new URL(NO_AUTH_LOGIN, request.url))
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/:path*',
-};
+  matcher: '/app/:path*',
+}
