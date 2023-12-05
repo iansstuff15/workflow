3e7ac9ccdd9c0a48a651237fc45caa482f98bb46
Diriangen Lempira Powell <dlpowell35@gmail.com>
2023-12-05 16:02:26 +0800

    feat: added route to auth signin

--- components/splash/splash.tsx
+++ components/splash/splash.tsx
@@ -1,33 +1,39 @@
-'use client';
-import Image from 'next/image';
-import AppIconWithText from '@/assets/logo-with-text-white.svg';
-import { Loader2Icon } from 'lucide-react';
-import { useEffect } from 'react';
-import { useRouter } from 'next-nprogress-bar';
-import { NO_AUTH_LOGIN } from '@/config/constants/routes/routes';
+'use client'
+import Image from 'next/image'
+import AppIconWithText from '@/assets/logo-with-text-white.svg'
+import { Loader2Icon } from 'lucide-react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { AUTH_LOGIN, NO_AUTH_LOGIN } from '@/config/constants/routes/routes'
+import { client } from '@/utilities/providers/backend/supabase'
 const Splash = () => {
-  const router = useRouter();
+  const router = useRouter()
+  const checkUser = async () => {
+    const {
+      data: { user },
+    } = await client.auth.getUser()
+    if (user) {
+      router.replace(AUTH_LOGIN)
+    } else {
+      router.replace(NO_AUTH_LOGIN)
+    }
+  }
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      router.replace(NO_AUTH_LOGIN);
-    }, 800);
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
+    checkUser()
+  }, [])
   return (
-    <div className=" background-animate grid place-items-center">
+    <div className=' background-animate grid place-items-center'>
       {' '}
       <Image
         src={AppIconWithText}
-        className="animate-pulse"
+        className='animate-pulse'
         width={450}
         height={450}
-        alt="workflow icon with text"
+        alt='workflow icon with text'
       />
-      <Loader2Icon color="white" className="animate-spin w-28" size={60} />
+      <Loader2Icon color='white' className='animate-spin w-28' size={60} />
     </div>
-  );
-};
+  )
+}
 
-export default Splash;
+export default Splash
