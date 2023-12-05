433f0780251b0584dace9310c6d16ee4b0a70f61
Diriangen Lempira Powell <dlpowell35@gmail.com>
2023-12-05 16:00:37 +0800

    fix: wrong imported useRouter

--- components/cards/signup/signup.card.tsx
+++ components/cards/signup/signup.card.tsx
@@ -1,96 +1,97 @@
-'use client';
-import AppButton from '@/components/button/appButtons';
-import GoogleIcon from '@/assets/google-icon.svg';
-import MicrosoftIcon from '@/assets/microsoft-icon.svg';
-import AppleIcon from '@/assets/apple-icon.svg';
-import AppIconWithText from '@/assets/icon-with-text.svg';
+'use client'
+import AppButton from '@/components/button/appButtons'
+import GoogleIcon from '@/assets/google-icon.svg'
+import MicrosoftIcon from '@/assets/microsoft-icon.svg'
+import AppleIcon from '@/assets/apple-icon.svg'
+import AppIconWithText from '@/assets/icon-with-text.svg'
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
   CardContent,
-} from '@/components/ui/card';
-import { loginProps } from '@/data/interface/card/login';
-import Image from 'next/image';
-import { useRouter } from 'next-nprogress-bar';
-import { NO_AUTH_LOGIN } from '@/config/constants/routes/routes';
+} from '@/components/ui/card'
+import { loginProps } from '@/data/interface/card/login'
+import Image from 'next/image'
+
+import { NO_AUTH_LOGIN } from '@/config/constants/routes/routes'
+import { useRouter } from 'next/navigation'
 
 const SignUpCard = ({ children }: loginProps) => {
-  const router = useRouter();
+  const router = useRouter()
   return (
-    <Card className="w-10/12 mx-auto my-auto">
+    <Card className='w-10/12 mx-auto my-auto'>
       <CardHeader>
         <CardTitle>
           <Image
             src={AppIconWithText}
             width={240}
             height={240}
-            alt="workflow-logo"
+            alt='workflow-logo'
           />
           <br />
           Register
         </CardTitle>
         <CardDescription>Start creating your account</CardDescription>
       </CardHeader>
-      <CardContent className="grid gap-4">
+      <CardContent className='grid gap-4'>
         {children}
         <AppButton
           variant={'secondary'}
-          label="Sign in"
+          label='Sign in'
           onClick={() => {
-            router.push(NO_AUTH_LOGIN);
+            router.push(NO_AUTH_LOGIN)
           }}
         />
-        <p className="mx-auto">or register with</p>
+        <p className='mx-auto'>or register with</p>
 
-        <div className="grid grid-cols-3 gap-2">
+        <div className='grid grid-cols-3 gap-2'>
           <AppButton
             variant={'outline'}
             onClick={() => {}}
-            label="Google"
+            label='Google'
             icon={
               <Image
                 src={GoogleIcon}
-                alt="google-icon-svg"
+                alt='google-icon-svg'
                 width={12}
                 height={12}
-                className="mr-1"
+                className='mr-1'
               />
             }
           />
           <AppButton
             variant={'outline'}
             onClick={() => {}}
-            label="Apple"
+            label='Apple'
             icon={
               <Image
                 src={AppleIcon}
-                alt="google-icon-svg"
+                alt='google-icon-svg'
                 width={12}
                 height={12}
-                className="mr-1"
+                className='mr-1'
               />
             }
           />
           <AppButton
             variant={'outline'}
             onClick={() => {}}
-            label="Microsoft"
+            label='Microsoft'
             icon={
               <Image
                 src={MicrosoftIcon}
-                alt="google-icon-svg"
+                alt='google-icon-svg'
                 width={12}
                 height={12}
-                className="mr-1"
+                className='mr-1'
               />
             }
           />
         </div>
       </CardContent>
     </Card>
-  );
-};
+  )
+}
 
-export default SignUpCard;
+export default SignUpCard
