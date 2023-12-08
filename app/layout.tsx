8cfa7b3ed4e9f70ee893b1d979771d46fe2449c7
Diriangen Lempira Powell <dlpowell35@gmail.com>
2023-12-08 22:20:32 +0800

    chore: added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { NextUIProvider } from '@nextui-org/system'
 import CoreProvider from '@/utilities/providers/core.provider'
 import { SupabaseProvider } from '@/utilities/providers/backend/supabase'
 import AppHeader from '@/components/header/header.component'
-
+import { SpeedInsights } from '@vercel/speed-insights/next'
 export const metadata: Metadata = appMetadata
 
 export default function RootLayout({
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang='en'>
       <body>
         <main>
+          <SpeedInsights />
           <SupabaseProvider>
             <CoreProvider>
               <AuthProvider>
