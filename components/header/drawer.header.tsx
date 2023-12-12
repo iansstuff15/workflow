c52e3f69a517aacea6b4f7d9248ac88f78ee628e
Diriangen Lempira Powell <dlpowell35@gmail.com>
2023-12-13 05:32:33 +0800

    feat: updated header content

--- components/header/drawer.header.tsx
+++ components/header/drawer.header.tsx
@@ -10,7 +10,21 @@ import {
 } from '@/components/ui/sheet'
 import AppButton from '../button/appButtons'
 import { Menu } from 'lucide-react'
+import ProjectItems from './menu-items/project-item'
+import AboutMeItem from './menu-items/about-me-item'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+} from '../ui/navigation-menu'
+import { ScrollArea } from '../ui/scroll-area'
+import Logo from '@/assets/icon.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+import { NO_AUTH_LOGIN } from '@/config/constants/routes/routes'
+import { useRouter } from 'next/navigation'
 const HeaderDrawer = () => {
+  const router = useRouter()
   return (
     <Sheet>
       <SheetTrigger asChild className='place-items-center'>
@@ -20,8 +34,43 @@ const HeaderDrawer = () => {
           className=' inline xl:hidden'
         />
       </SheetTrigger>
+
       <SheetContent side={'left'}>
-        <h1>App Drawer</h1>
+        <SheetHeader>
+          <Link href={'/'} className='flex gap-2 items-center'>
+            <Image src={Logo} alt='Workflow logo' width={30} height={30} />
+            <label className='text-white  mix-blend-difference font-bold text-md'>
+              Workflow
+            </label>
+          </Link>
+          <div className=' block md:hidden py-2 space-y-2'>
+            <div className='bg-white/70  backdrop-blur-lg grid space-y-2'>
+              <AppButton
+                label='Login'
+                block
+                onClick={() => {
+                  router.push(NO_AUTH_LOGIN)
+                }}
+              />
+              <AppButton
+                variant={'outline'}
+                label='Request demo'
+                block
+                className=' bg-transparent text-white  mix-blend-difference'
+              />
+            </div>
+            <AppButton
+              variant={'ghost'}
+              label='Contact me'
+              block
+              className='text-white  mix-blend-difference'
+            />
+          </div>
+        </SheetHeader>
+        <ScrollArea className='h-full'>
+          <ProjectItems />
+          <AboutMeItem />
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   )
--- components/header/menu.component.tsx
+++ components/header/menu.component.tsx
@@ -21,33 +21,8 @@ import {
   linkedInProfileLink,
   portfolioProjectsLink,
 } from '@/config/constants/links/links'
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'My Portfolio',
-    href: portfolioLink,
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'My Linkedin',
-    href: linkedInProfileLink,
-    description:
-      'Software Engineer, DOST-SEI scholar, AWS certified cloud practitioner, full stack react and flutter developer.',
-  },
-  {
-    title: 'My CV',
-    href: cvLink,
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Other Projects',
-    href: portfolioProjectsLink,
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-]
+import AboutMeItem from './menu-items/about-me-item'
+import ProjectItems from './menu-items/project-item'
 
 export function MenuHeader() {
   const path = usePathname()
@@ -59,41 +34,7 @@ export function MenuHeader() {
             The Project
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-              <li className='row-span-3'>
-                <NavigationMenuLink asChild className=''>
-                  <a
-                    className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-r from-blue-600 to-indigo-600 text-white no-underline outline-none focus:shadow-md py-3 px-4'
-                    href='/'
-                  >
-                    <div className='mt-4 text-xs font-medium'>
-                      current project
-                    </div>
-                    <div className='mb-2 text-xl font-medium'>Workflow</div>
-                    <p className='text-xs '>
-                      Is a personal project owned by Diriangen Lempira
-                      (nickname: Ian) Powell to showcase skills in web
-                      development. This site uses supabase for its backend and a
-                      combination of shadcn, nextui, tailwind css, and tremor
-                      for the front end
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-
-              <ListItem href='/docs' title='Git Repository'>
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href='/docs/installation' title='Git History'>
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem
-                href='/docs/primitives/typography'
-                title='Send Feedback'
-              >
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
+            <ProjectItems />
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
@@ -101,18 +42,7 @@ export function MenuHeader() {
             About Me
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
-              {components.map(component => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                  target='_blank'
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
+            <AboutMeItem />
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
@@ -132,27 +62,25 @@ export function MenuHeader() {
   )
 }
 
-const ListItem = React.forwardRef<
+export const ListItem = React.forwardRef<
   React.ElementRef<'a'>,
   React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-            className,
-          )}
-          {...props}
-        >
-          <div className='text-sm font-medium leading-none'>{title}</div>
-          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
+      <a
+        ref={ref}
+        className={cn(
+          'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+          className,
+        )}
+        {...props}
+      >
+        <div className='text-sm font-medium leading-none'>{title}</div>
+        <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+          {children}
+        </p>
+      </a>
     </li>
   )
 })
