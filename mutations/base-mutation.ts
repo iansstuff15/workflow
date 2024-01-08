af0ff0d04cdbb91bcba5736dc04b6e105425af48
Diriangen Lempira Powell <dlpowell35@gmail.com>
2024-01-09 04:49:10 +0800

    chore: prisma and mutations

---
+++ mutations/base-mutation.ts
@@ -0,0 +1,52 @@
+import {
+  deleteProps,
+  insertProps,
+  readAllProps,
+  updateProps,
+} from '@/data/interface/app-service/app-service'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+
+import { cookies } from 'next/headers'
+import { supabase } from '@/utilities/providers/backend/supabase'
+
+export class BaseMutation {
+  cookieStore = cookies()
+  supabase = createRouteHandlerClient({ cookies: () => this.cookieStore })
+  database: string = ''
+  constructor(database: string) {
+    this.database = database
+  }
+  readAll = async ({}: readAllProps) => {
+    return await this.supabase.from(this.database).select('*')
+  }
+  subscribe = ({}: readAllProps) => {
+    return this.supabase
+      .channel('custom-all-channel')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: this.database },
+        payload => {
+          console.log('Change received!', payload)
+        },
+      )
+      .subscribe()
+  }
+  insert = async ({ data, database }: insertProps) => {
+    return await database.from(this.database).insert(data).select()
+  }
+
+  update = async ({ data, searchKey, searchValue }: updateProps) => {
+    return await this.supabase
+      .from(this.database)
+      .update(data)
+      .eq(searchKey, searchValue)
+      .select()
+  }
+
+  deleteData = async ({ searchKey, searchValue }: deleteProps) => {
+    return await this.supabase
+      .from(this.database)
+      .delete()
+      .eq(searchKey, searchValue)
+  }
+}
