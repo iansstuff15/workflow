d13d30c13f9e14543cffdfb7a834ce38da8c0729
Diriangen Lempira Powell <dlpowell35@gmail.com>
2024-01-14 03:33:07 +0800

    fix: too long to display current user info

--- lib/utilities/providers/backend/supabase.tsx
+++ lib/utilities/providers/backend/supabase.tsx
@@ -1,24 +1,29 @@
 'use client'
+import { showError } from '@/lib/config/message/message.config'
 import { WrapperProps } from '@/lib/data/interface/layout/layout'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-import { Session, User, createClient } from '@supabase/supabase-js'
+import { AuthUser, Session, User, createClient } from '@supabase/supabase-js'
 import { createContext, useContext, useEffect, useState } from 'react'
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClientComponentClient({
+  supabaseKey: supabaseAnonKey,
+  supabaseUrl: supabaseUrl,
+})
 export const client = {
   auth: supabase.auth,
 }
 
 const SupabaseContext = createContext<{
   session: Session | null | undefined
-  user: User | null | undefined
+  user: AuthUser | null | undefined
   signOut: () => void
 }>({ session: null, user: null, signOut: () => {} })
 
 export const SupabaseProvider = ({ children }: WrapperProps) => {
-  const [user, setUser] = useState<User>()
+  const [user, setUser] = useState<AuthUser>()
   const [session, setSession] = useState<Session | null>()
   const [loading, setLoading] = useState(true)
 
@@ -28,9 +33,22 @@ export const SupabaseProvider = ({ children }: WrapperProps) => {
         data: { session },
         error,
       } = await supabase.auth.getSession()
-      if (error) throw error
+      if (error) {
+        showError({ message: error.message })
+      }
       setSession(session)
       setUser(session?.user)
+      if (session) {
+        let { data, error } = await supabase
+          .from('employee')
+          .select('*')
+          .eq('id', session.user.id)
+        console.log('data')
+        console.log(data)
+        if (error) {
+          showError({ message: error.message })
+        }
+      }
       setLoading(false)
     }
 
@@ -43,11 +61,7 @@ export const SupabaseProvider = ({ children }: WrapperProps) => {
     )
 
     setData()
-
-    return () => {
-      listener?.subscription.unsubscribe()
-    }
-  }, [])
+  }, [supabase])
 
   const value = {
     session,
